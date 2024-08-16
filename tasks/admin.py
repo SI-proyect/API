8ee1ede77a251fa086e2d8ae70601417dce872c6
EsGarciaC <esgarciac00@gmail.com>
2024-08-15 20:26:02 -0500

    Duparada base de datos a algo más manejable. Añadido usuario a la vista de admin y falta agregar campos de las declaraciones en la base de datos.

--- tasks/admin.py
+++ tasks/admin.py
@@ -1,3 +1,72 @@
 from django.contrib import admin
-
+from .models import User
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm
+from django.contrib.auth.admin import UserAdmin
 # Register your models here.
+
+class CustomUserCreationForm(UserCreationForm):
+    class Meta:
+        model = User
+        fields = ("email",)
+
+
+class CustomUserChangeForm(UserChangeForm):
+    class Meta:
+        model = User
+        fields = ("email",)
+
+
+class CustomUserAdmin(UserAdmin):
+    """
+    Class to customize the user administration panel
+    """
+
+    model = User
+    add_form = CustomUserCreationForm
+    form = CustomUserChangeForm
+
+    list_display = (
+            "id",
+            "cc",
+            "email",
+            "name",
+            "last_name",
+            "created_at",
+            "is_staff",
+            "is_active",
+        )
+    list_filter = (
+        "is_staff",
+        "is_active",
+    )
+    fieldsets = (
+        ("Information", {"fields": ("email", "password", "last_name")}),
+        (
+            "Permissions",
+            {"fields": ("is_staff", "is_active", "groups", "user_permissions")},
+        ),
+    )
+    add_fieldsets = (
+        (
+            None,
+            {
+                "classes": ("wide",),
+                "fields": (
+                    "cc",
+                    "email",
+                    "last_name",
+                    "password1",
+                    "password2",
+                    "is_staff",
+                    "is_active",
+                    "groups",
+                    "user_permissions",
+                ),
+            },
+        ),
+    )
+    search_fields = ("email", "name", "last_name")
+    ordering = ("id",)
+
+
+admin.site.register(User, CustomUserAdmin)
--- tasks/models.py
+++ tasks/models.py
@@ -37,11 +37,13 @@ class User(AbstractBaseUser, PermissionsMixin):
 
 class Client(models.Model):
     cc = models.IntegerField(default=0, unique=True)
+    nit = models.IntegerField(default=0, unique=True)
     name = models.CharField(max_length=100, default='null')
     address = models.CharField(max_length=100, default='null')
     telephone = models.IntegerField(default=0)
     mail = models.CharField(max_length=100, default='null')
     user = models.ForeignKey(User, on_delete=models.CASCADE)
+    fiscal_responsibilities = models.BooleanField(default=False)
     extra = models.CharField(max_length=300, default='null')
     created_at = models.DateTimeField(auto_now_add=True) 
     updated_at = models.DateTimeField(auto_now=True)
@@ -54,81 +56,15 @@ class Rut(models.Model):
     client = models.ForeignKey(Client, on_delete=models.CASCADE)
     nit = models.IntegerField(default=0, unique=True)
     check_digit = models.IntegerField(default=0) # Numero de verificacion
+    primary_economic_activity = models.IntegerField(default=0)
+    secondary_economic_activity = models.IntegerField(default=0)
     date = models.DateField(default='null') # y m d
     rut_type = models.CharField(max_length=100, default='null')
 
     def __str__(self) -> str:
         return 'Hola'
 
-class Fiscal_Responsibilities(models.Model):
-    rut = models.ForeignKey(Rut, on_delete=models.CASCADE)
-
-    def __str__(self) -> str:
-        return self.rut
-
-class Responsibilities(models.Model):
-    fiscal_responsabilities = models.ForeignKey(Fiscal_Responsibilities, on_delete=models.CASCADE)
-    description = models.CharField(max_length=100, default='null')
-    created_at = models.DateTimeField(auto_now_add=True) 
-    updated_at = models.DateTimeField(auto_now=True)
-    enabled = models.BooleanField(default=True)
-
-    def __str__(self) -> str:
-        return self.description
-    
-class Economic_activity(models.Model):
-    rut = models.ForeignKey(Rut, on_delete=models.CASCADE)
-
 class Declaration(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    # declaration_type = models.
-    state = models.CharField(max_length=100, default='null')
-    date = models.DateField(default='null')
-    description = models.CharField(max_length=100, default='null')
-    created_at = models.DateTimeField(auto_now_add=True) 
-    updated_at = models.DateTimeField(auto_now=True)
-    enabled = models.BooleanField(default=True)
+    client = models.ForeignKey(Client, on_delete=models.CASCADE, default='null')
+    ## TODO: Agregar campos para datos que vamos a sacar del pdf de las declaraciones (Jerson)
 
-# Patrimonio
-class Heritage(models.Model):
-    declaration = models.ForeignKey(Declaration, on_delete=models.CASCADE)
-    date = models.DateField(default='null')
-    # Patrimonio liquido = Patrimonio Bruto - Patrimonio Pasivo
-    liquid_equity = models.IntegerField(default=0)
-    heritage_type = models.CharField(max_length=100, default='null')
-
-# Pasive
-class Pasive_Heritage(models.Model):
-    heritage = models.ForeignKey(Heritage, on_delete=models.CASCADE)
-    # Deudas financieras
-    financial_debts = models.IntegerField(default=0)
-    # Deudas comerciales
-    business_debts = models.IntegerField(default=0)
-    # Deudas por impuestos
-    tax_debts = models.IntegerField(default=0)
-    # Otras deudas
-    other_debts = models.IntegerField(default=0)
-    # Pasivos inexistentes
-    nonexistent_liabilities = models.IntegerField(default=0)
-    # Total
-    total = models.IntegerField(default=0)
-
-class Brute_Heritage(models.Model):
-    # titulos
-    titles = models.CharField(max_length=100, default='null')
-    # inversiones
-    investments = models.CharField(max_length=100, default='null')
-    # Acciones
-    stock = models.CharField(max_length=100, default=100)
-    # Inventarios
-    inventories = models.CharField(max_length=100, default=100)
-    # Cuentas por cobrar
-    accounts_receivable = models.CharField(max_length=100, default=100)
-    # Activos fijos
-    fixed_assets = models.IntegerField(default=0)
-    # Otros activos
-    other_assets = models.IntegerField(default=0)
-    # Activos omitidos
-    omitted_assets = models.IntegerField(default=0)
-    # Total
-    total = models.IntegerField(default=0)
